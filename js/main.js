5746ccc4ae07ff81ddb0bb0e5bd8e9e0edd3c249
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2021-06-08 20:44:13 -0300

    criação da pagina index.html

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+
+/*
+console.log(setReplace("Vai Japão", "Japão", "Brasil"))
+console.log(soma(5, 10))
+
+
+var d = new Date()
+console.log(d.getDay())
+console.log(d.getHours())
+console.log(d.getMinutes())
+
+var count
+    for (count = 0; count <= 7; count++) {
+        console.log(count)
+    }
+
+var count = 0;
+while(count <= 5) {
+    console.log(count)
+    count = count + 1
+}
+
+var idade = prompt("Qual sua idade?")
+if (idade >= 18) {
+    console.log("maior de idade")
+} else {
+    console.log("menor de idade")
+}
+
+var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
+console.log(frutas[1].nome)
+
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta)
+//alert(fruta.cor)
+
+/*
+var lista = ["maçã", "pêra", "laranja"]
+//lista.pop()
+lista.push("uva")
+console.log(lista)
+console.log(lista.toString())
+console.log(lista.join(" | "))
+
+/*alert("meu primeiro js")
+var nome = "Mariana Nogueira"
+var idade = 30
+var idade2 = 10
+var frase = "Japão é o melhor time do mundo"
+
+console.log(nome)
+console.log(idade + idade2)
+console.log(frase.toLowerCase())
+//console.log(frase.replace("Japão", "Brasil"))
+*/
